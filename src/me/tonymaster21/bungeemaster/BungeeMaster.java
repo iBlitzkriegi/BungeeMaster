2ae0845a69ed394adad81825de359849003c212e
Blitz <iBlitzkriegi@users.noreply.github.com>
2017-10-24 21:44:49 -0400

    Added proper plugin checks

--- src/me/tonymaster21/bungeemaster/BungeeMaster.java
+++ src/me/tonymaster21/bungeemaster/BungeeMaster.java
@@ -16,16 +16,16 @@ public class BungeeMaster extends JavaPlugin {
     @Override
     public void onEnable(){
         getLogger().info("For help with BungeeMaster, please visit https://bungeemaster.tonymaster21.me");
-        if (Bukkit.getPluginManager().getPlugin("Skript") == null) {
-            getLogger().info("Skript is not installed on your server. This plugin will not work!");
-        }
-        if (Bukkit.getPluginManager().getPlugin("BungeeBridgeC") == null) {
-            getLogger().info("BungeeBridgeClient is not installed on your server. This plugin will not work!");
-        }
-        if (Bukkit.getPluginManager().getPlugin("Skript") != null) {
-            Skript.registerAddon(this);
-            addonInstance = this;
-            getAddonInstance.loadClasses("me.tonymaster21.bungeemaster", "effects", "expressions");
+        if (Bukkit.getPluginManager().getPlugin("BungeeBridgeC") != null) {
+            if (Bukkit.getPluginManager().getPlugin("Skript") != null) {
+                Skript.registerAddon(this);
+                addonInstance = this;
+                getAddonInstance.loadClasses("me.tonymaster21.bungeemaster", "effects", "expressions");
+            }else{
+                getLogger().info("You must have Skript installed in order to use this plugin.")
+            }
+        }else{
+                getLogger().info("BungeeBridgeClient is not installed on your server. This plugin will not work!");
         }
     }
     public static SkriptAddon getAddonInstance(){
